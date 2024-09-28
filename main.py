a87700a1e8206c10379df18ddec378586ddcdbc9
smhsimon <simonsung2023@u.northwestern.edu>
2024-09-28 09:15:22 -0700

    comment cleanup

--- main.py
+++ main.py
@@ -5,25 +5,7 @@ import settings
 import initialize
 import handle_input
 
-date = settings.curr_full_date
-
-name_input = 'Amy'
-normal_transactions = ([1, 2, 3, 4, 5, 6, 7, 8], True)
-sns_transactions = ([8, 7, 6, 5, 4, 3, 2, 1], True)
-
-# df = pd.read_csv('employee data\Lang\Lang_biweekly.csv')
-# if date in df['date'].values:
-#     print(True)
-# else:
-#     print(False)
-
-# if not (os.path.exists('employee data\\Lang\\Lang_biweekly.csv')) or date not in df['date'].values:
-# if not os.path.exists('employee data\\Lang\\Lang_biweekly.csv'):
-
 initialize.initialize(True)
-
-# else:
-#     handle_input.employee_input(name_input, normal_transactions, sns_transactions)
 
 # $env:FLASK_APP = "interface.py"
 # flask run --debug
--- settings.py
+++ settings.py
@@ -6,11 +6,11 @@ curr_day = int(datetime.datetime.today().strftime('%d'))
 curr_year = int(datetime.datetime.today().strftime('%Y'))
 curr_full_date = (datetime.datetime.today().strftime('%B') + ' ' + datetime.datetime.today().strftime('%d') + ', ' + datetime.datetime.today().strftime('%Y'))
 
-curr_month = 10
-curr_month_str = 'September'
-curr_day = 1
-curr_year = 2024
-curr_full_date = 'October 1, 2024'
+# curr_month = 10
+# curr_month_str = 'September'
+# curr_day = 1
+# curr_year = 2024
+# curr_full_date = 'October 1, 2024'
 
 
 employees = sorted(['Lang', 'Dennis', 'Linda', 'Tamy', 'Mindy', 'Amy', 'May', 'Ruby', 'Lucy', 'Holly', 'Kelly', 'Thanh'])
