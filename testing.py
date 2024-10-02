3fd4002168c9d569448d2c943becb7df0d3a22ec
smhsimon <simonsung2023@u.northwestern.edu>
2024-10-02 07:01:08 -0700

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-string = '35, 35, 35, 35, 35, 35, 35, 35, '
-
-normal = ''
-normal_raw = string
-if normal_raw != '':
-    normal = [int(x) for x in normal_raw.strip(', ').split(',')]
-proceed_normal = (normal, normal != '' and type(normal) != str)
-
-print(proceed_normal)
